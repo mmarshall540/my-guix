56c93f4ba6be6f251baae0d7cdaef48f82ecbc3f
Martin Marshall <law@martinmarshall.com>
2022-12-04 02:00:21 -0500

    Wrap elements in define inside of package form in emacs-meow.scm .

--- my/packages/emacs-meow.scm
+++ my/packages/emacs-meow.scm
@@ -2,14 +2,15 @@
 
 ;; Custom package definition to take advantage of recent bug fixes.
 (define emacs-meow-1.4.3
-  (inherit emacs-meow)
-  (version "1.4.3")
-  (source
-   (origin
-    (method git-fetch)
-    (uri (git-reference
-          (url "https://github.com/meow-edit/meow")
-          (commit "da8143e108617bed89669ac34e43d092a0a0b9b4")))
-    (file-name (git-file-name name version))
-    (sha256
-     (base32 "1wxk3xf2d4q2vp9bh1ddj8g32pmrjdsb8rqfdmj2ca0pp5m9l6x4")))))
+  (package
+   (inherit emacs-meow)
+   (version "1.4.3")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/meow-edit/meow")
+           (commit "da8143e108617bed89669ac34e43d092a0a0b9b4")))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "1wxk3xf2d4q2vp9bh1ddj8g32pmrjdsb8rqfdmj2ca0pp5m9l6x4"))))))
